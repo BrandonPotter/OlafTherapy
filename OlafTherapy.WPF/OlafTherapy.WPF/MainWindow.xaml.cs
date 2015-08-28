6c65da81b28ca886bc571ba3fc9e75960a6c4b40
Brandon Potter <brandon@brandonpotter.com>
2015-08-28 16:05:56 -0400

    Initial version

---
+++ OlafTherapy.WPF/OlafTherapy.WPF/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OlafTherapy.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            pbLeft.StateChanged += OnStateChanged;
+            pbRight.StateChanged += OnStateChanged;
+        }
+
+        bool _isPlaying = false;
+        private void OnStateChanged()
+        {
+            if (_isPlaying) { return; }
+            if (pbLeft.IsPressed && pbRight.IsPressed)
+            {
+                System.Threading.ThreadPool.QueueUserWorkItem(DoReward);
+            }
+        }
+
+        private void DoReward(object state)
+        {
+            _isPlaying = true;
+
+            this.Dispatcher.Invoke(new Action(() => {
+                pbLeft.Visibility = Visibility.Hidden;
+                pbRight.Visibility = Visibility.Hidden;
+                olaf.Visibility = Visibility.Visible;
+            }));
+
+            MediaPlayer mp = new MediaPlayer();
+            mp.Open(new Uri(new System.IO.FileInfo(".\\OlafWarmHugs.mp3").FullName));
+            mp.Play();
+            System.Threading.Thread.Sleep(6000);
+
+            this.Dispatcher.Invoke(new Action(() => {
+                Reset();
+            }));
+
+            _isPlaying = false;
+        }
+
+        Random _r = new Random(DateTime.Now.Millisecond);
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            pbLeft.Visibility = Visibility.Visible;
+            pbRight.Visibility = Visibility.Visible;
+            pbLeft.Reset();
+            pbRight.Reset();
+            Canvas.SetTop(pbLeft, _r.Next(0, Convert.ToInt32(canvasLeft.ActualHeight - pbLeft.ActualHeight)));
+            Canvas.SetLeft(pbLeft, _r.Next(0, Convert.ToInt32(canvasLeft.ActualWidth - pbLeft.ActualWidth)));
+            Canvas.SetTop(pbRight, _r.Next(0, Convert.ToInt32(canvasRight.ActualHeight - pbRight.ActualHeight)));
+            Canvas.SetLeft(pbRight, _r.Next(0, Convert.ToInt32(canvasRight.ActualWidth - pbRight.ActualWidth)));
+            
+            this.olaf.Visibility = Visibility.Hidden;
+        }
+
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            //base.OnPreviewMouseRightButtonDown(e);
+        }
+
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            base.OnPreviewMouseRightButtonUp(e);
+        }
+    }
+}
---
+++ OlafTherapy.WPF/OlafTherapy.WPF/PushButton.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OlafTherapy.WPF
+{
+    /// <summary>
+    /// Interaction logic for PushButton.xaml
+    /// </summary>
+    public partial class PushButton : UserControl
+    {
+        public PushButton()
+        {
+            InitializeComponent();
+            this.thumbCircle.TouchDown += ThumbCircle_TouchDown;
+            this.thumbCircle.TouchUp += ThumbCircle_TouchUp;
+        }
+
+        public event Action StateChanged;
+
+        public bool IsPressed
+        {
+            get; set;
+        }
+
+        public void Reset()
+        {
+            this.thumbCircle.Fill = Brushes.Yellow;
+            IsPressed = false;
+        }
+
+        private void ThumbCircle_TouchUp(object sender, TouchEventArgs e)
+        {
+            IsPressed = false;
+            this.thumbCircle.Fill = Brushes.Yellow;
+            FireStateChanged();
+        }
+
+        private void ThumbCircle_TouchDown(object sender, TouchEventArgs e)
+        {
+            IsPressed = true;
+            this.thumbCircle.Fill = Brushes.Lime;
+            FireStateChanged();
+        }
+
+        private void FireStateChanged()
+        {
+            if (StateChanged != null)
+            {
+                StateChanged();
+            }
+        }
+    }
+}
